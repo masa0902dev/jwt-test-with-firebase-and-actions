ee9206e80076648a03acdc2dd4aa0f9565407756
masa0902dev <masa0902dev@gmail.com>
2025-01-13 14:39:30 +0900

    express-rate-limit, Functionのエラーを修正
    express-rate-limit:
    validate: { id: false }を加えればエラーが解消される。
    レートリミットの動作確認はできたので、問題ないだろう。
    
    Functions:
    expressのapp.use, app.getの使い方を間違えていた。
    今のコードの状態がおよそ良い形だろう。
    今の状態→先にapp.get,.app.postなどでそのメソッドの特定パスに特定ルーターに適用し、最後にまとめてapp.useで全メソッドの各パスに各ルーターを適用。

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import { rateLimit } from "express-rate-limit";
 import admin from "firebase-admin";
-import serviceAccountKey from "../serviceAccountKey.json";
+import serviceAccountKey from "./serviceAccountKey.json";
 import { onRequest } from "firebase-functions/v2/https";
 // import { authenticateJWT } from "./middleware/jwt";
 
@@ -15,30 +15,35 @@ const app = express();
 app.use(cors({ origin: true }));
 app.use(helmet());
 app.use(express.json());
+
 // レートリミット: 5min に 300 リクエストまで。動作確認完了
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   limit: 300,
   message: "Sorry😭 Too many requests, please try again later.",
   statusCode: 429,
+  // NOTE: undefined ip address error: https://express-rate-limit.mintlify.app/reference/error-codes#err-erl-undefined-ip-address
+  validate: { ip: false },
 });
 
 import forecastRouter from "./routers/forecast";
 import temperatureRouter from "./routers/temperature";
 import estimationRouter from "./routers/estimation";
 
-// forecast
-// GET: rate-limit, POST: JWT
-app.get("/forecast", limiter as unknown as express.RequestHandler, forecastRouter);
-app.post("/forecast", forecastRouter);
-// temperature
-// GET: no middleware, POST: JWT
-app.get("/temperature", temperatureRouter);
-app.post("/temperature", temperatureRouter);
-// estimation
-// GET: JWT, POST: JWT
-app.get("/estimation", estimationRouter);
-app.post("/estimation", estimationRouter);
+// forecast: GET -> rate-limit, POST -> JWT
+app.get("/forecast", limiter);
+// app.post("/forecast", authenticateJWT);
+
+// temperature: GET -> nothing, POST -> JWT
+// app.post("/temperature", authenticateJWT);
+
+// estimation: GET -> JWT, POST -> JWT
+// app.get("/estimation", authenticateJWT);
+// app.post("/estimation", authenticateJWT);
+
+app.use("/forecast", forecastRouter);
+app.use("/temperature", temperatureRouter);
+app.use("/estimation", estimationRouter);
 
 // region for functions: https://firebase.google.com/docs/functions/locations?hl=ja&_gl=1*18o985t*_up*MQ..*_ga*MTU0NTI1MDM5My4xNzM2NzQwMzU4*_ga_CW55HF8NVT*MTczNjc0MDM1OC4xLjAuMTczNjc0MDM1OC4wLjAuMA..
 const region = "asia-northeast1";
