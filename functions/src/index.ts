d76a779888738f9e73da33dbbdd4b8b235ff6138
masa0902dev <masa0902dev@gmail.com>
2025-01-15 13:50:13 +0900

    WIP: デプロイしてJWTできるか確認

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,17 @@ import { rateLimit } from "express-rate-limit";
 import admin from "firebase-admin";
 import serviceAccountKey from "./serviceAccountKey.json";
 import { onRequest } from "firebase-functions/v2/https";
-// import { authJwt } from "./middleware/jwt";
+import authJwt from "./middlewares/jwt";
+import { config } from "dotenv";
+config();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount),
 });
+
+const region = "asia-northeast1";
+const baseUrl = "https://firestore-fzxwlwc6iq-an.a.run.app";
+// const baseUrl = `http://127.0.0.1:5001/jwt-test-with-actions/${region}/firestore`;
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -32,20 +38,18 @@ import estimationRouter from "./routers/estimation";
 
 // forecast: GET -> rate-limit, POST -> JWT
 app.get("/forecast", limiter);
-// app.post("/forecast", authJwt);
+app.post("/forecast", authJwt(baseUrl + "/forecast"));
 
 // temperature: GET -> nothing, POST -> JWT
-// app.post("/temperature", authJwt);
+app.post("/temperature", authJwt(baseUrl + "/temperature"));
 
 // estimation: GET -> JWT, POST -> JWT
-// app.get("/estimation", authJwt);
-// app.post("/estimation", authJwt);
+app.get("/estimation", authJwt(baseUrl + "/estimation"));
+app.post("/estimation", authJwt(baseUrl + "/estimation"));
 
 app.use("/forecast", forecastRouter);
 app.use("/temperature", temperatureRouter);
 app.use("/estimation", estimationRouter);
-
-const region = "asia-northeast1";
 
 export const firestore = onRequest({ region: region }, (req, res) => {
   app(req, res);
