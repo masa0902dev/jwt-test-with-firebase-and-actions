12f46bca8d7df56aa0aad121a6cdad3704197196
masa0902dev <masa0902dev@gmail.com>
2025-01-17 23:26:56 +0900

    値を.env, constantsに記載、ログ削除

---
+++ functions/src/constants.ts
@@ -0,0 +1,5 @@
+// Githubの公開鍵が置いてあるURL
+export const githubPublicKeyUrl =
+  "https://token.actions.githubusercontent.com/.well-known/jwks";
+
+export const jwtIssuer = "https://token.actions.githubusercontent.com";
